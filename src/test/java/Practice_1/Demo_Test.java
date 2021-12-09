8fb15160efb204712034eb99ac1be7a36a6afdb9
Nitin Jallandhra <Nitin Jallandhra@NitinJallandhra>
2021-12-09 15:03:53 +0530

    first commit

---
+++ src/test/java/Practice_1/Demo_Test.java
@@ -0,0 +1,12 @@
+package Practice_1;
+
+import org.testng.annotations.Test;
+
+public class Demo_Test {
+	@Test
+	public void CreateTest()
+	{
+		System.out.println("DemoPractice is Pass");
+		
+	}
+}
